484c7dbc11c02c723f08f0ca9462bfab042eac92
romulo lima <dhb.tecnologia@gmail.com>
2021-09-06 22:34:54 -0300

    Projeto criado

---
+++ src/test/java/io/zolthan31fps/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package io.zolthan31fps.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
